bc421bd9ba6bcaa6f72e6a01f5572531078e7a37
Georg <Georg.Braun92@gmail.com>
2022-03-19 09:55:45 +0100

    add bearer authentication boilerplate

--- backend/budget-backend/Program.cs
+++ backend/budget-backend/Program.cs
@@ -1,20 +1,71 @@
+using System.Text;
 using budget_backend.application;
 using budget_backend.Controllers;
-using budget_backend.Controllers.apiDto;
 using budget_backend.data;
 using budget_backend.endpoints;
+using budget_backend.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var jwtSettings = new JwtSettings();
+builder.Configuration.Bind(nameof(jwtSettings), jwtSettings);
+builder.Services.AddSingleton(jwtSettings);
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(jwtOptions =>
+{
+    // how the token looks like
+    jwtOptions.SaveToken = true;
+    jwtOptions.TokenValidationParameters = new TokenValidationParameters()
+    {
+        // validate the hash with the secret
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSettings.Secret)),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        RequireExpirationTime = false,
+        ValidateLifetime = true
+    };
+});
+
+
 builder.Services.AddScoped<IAccountService, AccountService>();
 
 builder.Services.AddDbContext<DataContext>(optionsBuilder => optionsBuilder.UseNpgsql(
     builder.Configuration["ConnectionStrings:Database"]));
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(x =>
+{
+    x.SwaggerDoc("v1", new OpenApiInfo(){Title = "Budget API", Version = "v1"});
+
+    var securityScheme = new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "bearer",
+        BearerFormat = "JWT"
+    };
+    
+    x.AddSecurityDefinition("Bearer", securityScheme);
+
+    x.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {securityScheme, Array.Empty<string>()}
+    });
+});
 
 var app = builder.Build();
 
@@ -26,7 +77,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-//app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapGet(Routes.GetAll, AccountEndpoints.GetAll);
 app.MapGet(Routes.GetAllAccounts, AccountEndpoints.GetAllAccounts);
