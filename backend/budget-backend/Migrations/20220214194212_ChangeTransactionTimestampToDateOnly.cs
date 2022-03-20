d209da88f25f8b94e8bb2140c0ed069f4ab80460
Georg <Georg.Braun92@gmail.com>
2022-03-20 09:48:43 +0100

    migrations

---
+++ backend/budget-backend/Migrations/20220214194212_ChangeTransactionTimestampToDateOnly.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace budget_backend.Migrations
+{
+    public partial class ChangeTransactionTimestampToDateOnly : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ backend/budget-backend/Migrations/20220320081707_New-Types.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace budget_backend.Migrations
+{
+    public partial class NewTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AccountTransactions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AccountEntryIdFrom = table.Column<Guid>(type: "uuid", nullable: false),
+                    AccountIdFrom = table.Column<Guid>(type: "uuid", nullable: false),
+                    AccountIdTo = table.Column<Guid>(type: "uuid", nullable: false),
+                    AccountEntryIdTo = table.Column<Guid>(type: "uuid", nullable: false),
+                    Timestamp = table.Column<DateOnly>(type: "date", nullable: false),
+                    Amount = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccountTransactions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BudgetaryItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BudgetaryItems", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BudgetEntries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    BudgetaryItemId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Amount = table.Column<double>(type: "double precision", nullable: false),
+                    Month = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BudgetEntries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Spendings",
+                columns: table => new
+                {
+                    AccountEntryId = table.Column<Guid>(type: "uuid", nullable: false),
+                    BudgetaryItemId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AccountId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Spendings", x => new { x.AccountEntryId, x.BudgetaryItemId });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AccountTransactions");
+
+            migrationBuilder.DropTable(
+                name: "BudgetaryItems");
+
+            migrationBuilder.DropTable(
+                name: "BudgetEntries");
+
+            migrationBuilder.DropTable(
+                name: "Spendings");
+        }
+    }
+}
