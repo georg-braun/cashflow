6b66839bf43635fa205f63ce9fd99d8fe1df9cdf
Georg <Georg.Braun92@gmail.com>
2022-03-30 20:45:55 +0200

    add scenario test skeleton

---
+++ backend/budget-backend-integration-tests/Scenarios.cs
@@ -0,0 +1,31 @@
+using Xunit;
+
+namespace budget_backend_integration_tests;
+
+public class Scenarios
+{
+    /// <summary>
+    ///     This test demonstrates a normal month with income, budgeted money and spendings.
+    /// </summary>
+    [Fact]
+    public void RegularMonthSceanario()
+    {
+        // Arrange
+        
+        // todo: add account
+        // todo: add budgetary item
+        // todo: add budget
+        // todo: reduce budget
+        // todo: add income
+        // todo: add budget
+        // todo: add spending
+        // todo: add income
+        
+        // Act
+        // Assert
+        
+        // todo: check available money in account
+        // todo: check available budget
+
+    }
+}
